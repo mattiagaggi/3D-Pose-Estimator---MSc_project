63f56f8665bfdd5c1449f05bfe94469b24bc551f
student <m.gaggi@hotmail.it>
2019-06-07 21:59:24 +0100

    first commit

---
+++ sample/utils/visualise.py
@@ -0,0 +1,11 @@
+
+print("kkk")
+import sys
+print(sys.path)
+h36m_dir="/../../../../../../../data_fast/Datasets/h36m/"
+
+file="s_01_act_02_subact_01_ca_01/s_01_act_02_subact_01_ca_01_000193.jpg"
+
+from PIL import Image
+img=Image.open(h36m_dir+file)
+img.show()
