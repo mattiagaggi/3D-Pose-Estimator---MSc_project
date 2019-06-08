95787b6d1f4a07ee44a6304927cd0c2cd3c3b170
student <m.gaggi@hotmail.it>
2019-06-08 19:11:54 +0100

    change visualisation

--- sample/utils/visualise.py
+++ sample/utils/visualise.py
@@ -1,12 +1,21 @@
 
 
-h36m_dir="/../../../../../../../data_fast/Datasets/h36m/"
+from data.directories_location import  h36m_location
+subdirfile="s_01_act_02_subact_01_ca_04/"
 
-file="s_01_act_02_subact_01_ca_01/s_01_act_02_subact_01_ca_01_000193.jpg"
+file="s_01_act_02_subact_01_ca_04_000002.jpg"
+gtfile="h36m_meta.mat"
 
 from PIL import Image
-img=Image.open(h36m_dir+file)
+import scipy.io as sio
 
+
+data=sio.loadmat(h36m_location+subdirfile+gtfile)
+points_3D=data['pose3d_world'] #3D numpy array
+cam_r=data['R']
+print(type(cam_r))
+print(cam_r)
+img=Image.open(h36m_location+subdirfile+file)
 img.show()
 
-print("jdjdkdkdk")
+print()
